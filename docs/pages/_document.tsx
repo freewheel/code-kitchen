b89aaaad306522209a5028860433f8b4e35b4e22
Peng Xiao <pengxiao@outlook.com>
2022-03-09 16:07:32 +0800

    docs: add GA

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,12 +1,41 @@
 import * as React from "react";
+
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === "production";
+    const initialProps = await Document.getInitialProps(ctx);
+    // Pass isProduction flag back through props
+    return { ...initialProps, isProduction };
+  }
+
   render() {
+    const { isProduction } = this.props as any;
     return (
       <Html>
         <Head>
           <link href="fonts/fonts.css" rel="stylesheet" />
+          {isProduction && (
+            <>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-KLHBQKELR6"
+              ></script>
+              <script
+                id="google-analytics"
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+        
+                  gtag('config', 'G-KLHBQKELR6');`,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
